cc75766fb09267c875cc9fff77011bcb06ab1cbf
Kenji Rikitake <kenji.rikitake@acm.org>
2014-09-22 01:11:28 +0900

    Re-merge sg2342/master pull request #5
    https://github.com/jj1bdx/erlang-freebsd-port/commit/cd602c22162a9c14b328844c8444b1421abf8889

---
+++ erlang-runtime17/files/extra-patch-erts_etc_common_erlc.c
@@ -0,0 +1,13 @@
+
+$FreeBSD: head/lang/erlang-runtime17/files/patch-erts_etc_common_erlc.c 340725 2014-01-22 17:40:44Z mat $
+
+--- erts/etc/common/erlc.c.orig
++++ erts/etc/common/erlc.c
+@@ -186,6 +186,7 @@
+      */
+ 
+     PUSH("-noinput");
++    PUSH2("-smp", "enable");
+     PUSH2("-mode", "minimal");
+     PUSH2("-boot", "start_clean");
+     PUSH3("-s", "erl_compile", "compile_cmdline");
